d8611cf59c35c7cdb0b8113765f7222fed4c2dfc
julietaMendez <jmendez1234@mail.fresnostate.edu>
2023-04-09 09:54:29 -0700

    program can run given rule or all rules

--- elementary_CA.py
+++ elementary_CA.py
@@ -7,8 +7,9 @@ def apply_rule(rule, iterations):
         return
 
     binary = format(rule, "0>8b") # convert rule to binary
-    binary = (list(map(int,binary))) # create list
-    patterns = [ # list of tuples
+    binary = (list(map(int,binary))) # convert binary num to list of int
+
+    patterns = [ # list of patterns as tuples
         (1,1,1),
         (1,1,0),
         (1,0,1),
@@ -18,41 +19,27 @@ def apply_rule(rule, iterations):
         (0,0,1),
         (0,0,0)
     ]
+    pattern_rules_dict = dict(zip(patterns, binary)) # assigns each pattern its respective value from the binary num
 
-    pattern_rules_dict = dict(zip(patterns, binary)) # assigns each pattern its respective value (0 or 1)
     row_length = iterations * 2 - 1 
-    first_row_start_index = int(row_length / 2)
-    initial_board = [0] * row_length
-    initial_board[first_row_start_index] = 1
+    first_row_start_index = int(row_length / 2 + 1) # index of the first block
+    initial_row = [0] * (row_length + 2) # board consists of 0s initially, need 2 extra 0s to determine next row
+    initial_row[first_row_start_index] = 1 # set first block
 
-    board.append(initial_board)
-    curr_row = initial_board
-    curr_row.insert(0, 0)
-    curr_row.append(0)
-
-    # repeat for every row
-    for i in range(0, iterations-1):
-        next_row = [0] * row_length
-        next_row_len = len(next_row)
-        print("Next row len: ",next_row_len, "curr row len: ", len(curr_row))
-        first_row_start_index  -= 1
-        value_index = 0
+    board = [] # create matrix
+    board.append(initial_row) 
+    
+    curr_row = initial_row
+    for i in range(0, iterations-1): 
+        next_row = [0] * row_length # create next row with initially all 0s
         stop_index = len(curr_row) - 2
 
         # Get pattern to create next row
         for j in range(0, len(curr_row)):
             if(j < stop_index): # should stop when there are less than 3 values left in the board
-                print("j", j)
                 pattern_tuple = tuple(curr_row[j:j+3]) # get patterns to create next row
                 value = pattern_rules_dict[pattern_tuple]
-                next_row[value_index] = value
-                
-                # FOR DEBUGGING PURPOSES
-                # print("value ", value, "value index ", value_index)
-                # print("curr row ", curr_row)
-                # print("next_row:   ", next_row)
-                
-                value_index += 1
+                next_row[j] = value
 
         board.append(next_row)
         curr_row = next_row
@@ -62,16 +49,20 @@ def apply_rule(rule, iterations):
     return board
 
 def show_board(board, rule):
-    plt.figure(figsize=(5,3), facecolor="pink")
+    plt.figure(figsize=(10,5), facecolor="pink")
     plt.imshow(board, cmap="Greys")
     plt.axis("off")
     plt.title(rule, fontsize=20)
     plt.show()
     plt.close()
 
-board = []
+def run_all_rules(iterations):
+    for i in range(0, 256):
+        board = apply_rule(i, iterations)
+        show_board(board, i) 
+
 rule = 30
-iterations = 5
-board = apply_rule(rule, iterations)
-# print(board)
-show_board(board, rule) 
+iterations = 500
+show_board(apply_rule(rule, iterations), rule)
+# run_all_rules(iterations)
+
