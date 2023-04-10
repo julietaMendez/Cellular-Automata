5d24d03dd4aea6db5699eb64c66f901d8a994956
julietaMendez <jmendez1234@mail.fresnostate.edu>
2023-04-10 07:01:18 -0700

    Fixed tail/black transition but still need to slow the transitions

--- wire_world.py
+++ wire_world.py
@@ -1,105 +1,86 @@
 import PySimpleGUI as psg
-
-''' 
-graph=psg.Graph(canvas_size=(700,300), graph_bottom_left=(0, 0), graph_top_Head=(700,300),
-    background_color='red', enable_events=True, drag_submits=True, key='graph')
-layout = [[graph], [psg.Button('LEFT'), psg.Button('Head'), psg.Button('UP'), psg.Button('DOWN')]]
-
-window = psg.Window('Graph test', layout, finalize=True)
-x1,y1 = 350,150
-circle = graph.draw_circle((x1,y1), 10, fill_color='black', line_color='white')
-rectangle = graph.draw_rectangle((50,50), (650,250), line_color='purple')
-while True:
-   event, values = window.read()
-   if event == psg.WIN_CLOSED:
-      break
-   if event == 'Head':
-      graph.MoveFigure(circle, 10, 0)
-   if event == 'LEFT':
-      graph.MoveFigure(circle, -10,0)
-   if event == 'UP':
-      graph.MoveFigure(circle, 0, 10)
-   if event == 'DOWN':
-      graph.MoveFigure(circle, 0,-10)
-   if event=="graph+UP":
-      x2,y2= values['graph']
-      graph.MoveFigure(circle, x2-x1, y2-y1)
-      x1,y1=x2,y2
-window.close()
-'''
+import time
 
 def count_head_neighbors(board, col, row):
     num_head_neighbors = 0
 
     if(col != 0): # ensure there is a col to the left (L)
-        if(board[col-1][row] == "Head"):
+        if(board[col-1][row] == "purple"):
+            print("L")
             num_head_neighbors += 1
 
     if(col != row_length - 1): # ensure there is a col to the right (R)
-        if(board[col+1][row] == "Head"):
+        if(board[col+1][row] == "purple"):
             num_head_neighbors += 1 
 
     # check U, UL, and UR
     if(row != row_length - 1): # ensure there is a row above
-        if(board[col][row+1] == "Head"): # (U)
+        if(board[col][row+1] == "purple"): # (U)
+            print("U")
             num_head_neighbors += 1 
         if(col != 0): # ensure there is a col to the left (UL)
-            if(board[col-1][row+1] == "Head"):
+            if(board[col-1][row+1] == "purple"):
                 num_head_neighbors += 1       
         if(col != row_length - 1): # ensure there is a col to the right (UR)
-            if(board[col+1][row+1] == "Head"):
+            if(board[col+1][row+1] == "purple"):
                 num_head_neighbors += 1 
 
     # check D, DL, and DR
     if(row != 0): # ensure there is a row below
-        if(board[col][row-1] == "Head"): # (D)
+        if(board[col][row-1] == "purple"): # (D)
             num_head_neighbors += 1 
         if(col != 0): # ensure there is a col to the left (DL)
-            if(board[col-1][row-1] == "Head"):
+            if(board[col-1][row-1] == "purple"):
                 num_head_neighbors += 1 
         if(col != row_length - 1): # ensure there is a col to the right (DR)
-            if(board[col+1][row-1] == "Head"):
+            if(board[col+1][row-1] == "purple"):
                 num_head_neighbors += 1 
 
     return num_head_neighbors
 
 def run(current_board):
-    iterations = 10
-    new_board = init_cells(current_board) # creates board for next iteration
+    new_board = init_cells() # creates board for next iteration
     x = 500
     BOX_SIZE = 15 #length is 33
 
+    # time.sleep(3) # doesn't work as intended yet
     graph = psg.Graph((x, x), (0, 0), (x, x), background_color='white', enable_events=True, key='-GRAPH-', visible=True)                  
     layout = [[graph], [psg.Button('Run', button_color="green", key='-DONE-'), 
                         psg.Button('Head', button_color="purple"), psg.Button('Tail', button_color="black"), 
                         psg.Button('Connector',button_color="orange"), psg.Button('Empty',button_color="white")]]
     window = psg.Window('Wire World by Julieta Mendez', layout, finalize=True)
 
-    for i in range(0,iterations):
-        # update board with new cells
-        for col in range(0,row_length):
-            for row in range(0, row_length):
+    # update board with new cells
+    for col in range(0,row_length):
+        for row in range(0, row_length):
 
-                if(current_board[col][row] == "purple"):
+            if(current_board[col][row] == "purple"):
+                # print("1")
+                graph.draw_rectangle((row * BOX_SIZE, col * BOX_SIZE), # top left, bottom right
+                    (row * BOX_SIZE + BOX_SIZE, col * (BOX_SIZE) + BOX_SIZE), line_color='white', fill_color="black")
+                new_board[col][row] = "black"
+
+            if(current_board[col][row] == "black"):
+                # print("2")
+                graph.draw_rectangle((row * BOX_SIZE, col * BOX_SIZE), # top left, bottom right
+                    (row * BOX_SIZE + BOX_SIZE, col * (BOX_SIZE) + BOX_SIZE), line_color='white', fill_color="orange")
+                new_board[col][row] = "orange"
+
+            if(current_board[col][row] == "orange"):
+                # print("3")
+                num_head_neighbors = count_head_neighbors(current_board, col, row)
+                print("nieghbors", num_head_neighbors)
+                if(num_head_neighbors == 1 or num_head_neighbors == 2):
+                    print("purple")
                     graph.draw_rectangle((row * BOX_SIZE, col * BOX_SIZE), # top left, bottom right
-                        (row * BOX_SIZE + BOX_SIZE, col * (BOX_SIZE) + BOX_SIZE), line_color='white', fill_color="black")
-                    new_board[col][row] = "Tail"
-
-                if(current_board[col][row] == "black"):
+                    (row * BOX_SIZE + BOX_SIZE, col * (BOX_SIZE) + BOX_SIZE), line_color='white', fill_color="purple")
+                    new_board[col][row] = "purple"
+                else:
                     graph.draw_rectangle((row * BOX_SIZE, col * BOX_SIZE), # top left, bottom right
-                        (row * BOX_SIZE + BOX_SIZE, col * (BOX_SIZE) + BOX_SIZE), line_color='white', fill_color="orange")
-                    new_board[col][row] = "Connector"
-
-                if(current_board[col][row] == "orange"):
-                    num_head_neighbors = count_head_neighbors(current_board, col, row)
-                    if(num_head_neighbors == 1 or num_head_neighbors == 2):
-                        graph.draw_rectangle((row * BOX_SIZE, col * BOX_SIZE), # top left, bottom right
-                        (row * BOX_SIZE + BOX_SIZE, col * (BOX_SIZE) + BOX_SIZE), line_color='white', fill_color="purple")
-                        new_board[col][row] = "Head"
-                    else:
-                        graph.draw_rectangle((row * BOX_SIZE, col * BOX_SIZE), # top left, bottom right
-                        (row * BOX_SIZE + BOX_SIZE, col * (BOX_SIZE) + BOX_SIZE), line_color='white', fill_color="orange")
-                        new_board[col][row] = "Connector"
+                    (row * BOX_SIZE + BOX_SIZE, col * (BOX_SIZE) + BOX_SIZE), line_color='white', fill_color="orange")
+                    new_board[col][row] = "orange"
+    
+    return new_board
 
 def create_init_board(board): # user places cells
     x = 500
@@ -126,13 +107,8 @@ def create_init_board(board): # user places cells
         # initally, user draws
         if event == '-GRAPH-':
             graph.draw_rectangle((row * BOX_SIZE, col * BOX_SIZE),
-                (row * BOX_SIZE + BOX_SIZE, col * (BOX_SIZE) + BOX_SIZE), line_color='white', fill_color=color)
-            
+                (row * BOX_SIZE + BOX_SIZE, col * (BOX_SIZE) + BOX_SIZE), line_color='white', fill_color=color)          
             board[col][row] = color
-            print(board)
-            # ------- print for DEBUGGING PURPOSES -------------------------
-            # for i in range(0, row_length):
-            #     print(i, col, ": ", board[i][row]) #prints row -----------
 
         if event == 'Empty': 
             color = "white"
@@ -142,23 +118,20 @@ def create_init_board(board): # user places cells
             color = "black"
         if event == 'Connector':
             color = "orange"
-            print("fucing orange")
         if event == "Run":
-            print("wtf")
-            run(board) 
+            for i in range(0,iterations):
+                board = run(board) # returns new board
 
     window.close()
 
-def init_cells(board):
+def init_cells():
+    board = []
     for i in range(0, row_length):
         next_row = ["Empty"] * row_length # set all cells as empty initially
         board.append(next_row)
     return board
 
-
+iterations = 5
 row_length = 34 #if size is 15
-initial_board = []
-initial_board = init_cells(initial_board) # initiate cells as Empty
-
+initial_board = init_cells() # initiate cells as Empty
 create_init_board(initial_board) # player sets cells
-# run(initial_board)
